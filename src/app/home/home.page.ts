c0046836338538e1a08647b5acd2c231d8959b1d
dianaavilaao <165206809+dianaavilaao@users.noreply.github.com>
2024-09-06 22:32:20 -0400

    Co-authored-by: nicolenavarrorojas <nicolenavarrorojas@users.noreply.github.com>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  nombre!: string;
+  apellido!: string;
+  fechanacimiento!: string;
+  
+  eventos = [
+    { tipo: 'Concierto', precio: 10000 },
+    { tipo: 'Exhibicion', precio: 50000 },
+    { tipo: 'Torneo', precio: 30000 }
+  ];
 
+  entrada = [
+    { tipo: 'normal', precio: 10000 },
+    { tipo: 'exhibicion', precio: 50000 },
+    { tipo: 'torneo', precio: 30000 }
+  ];
+
+  numentradas!: number;
+
+  selectedEvent!: string;
+  precio!: number;
+
+  alertButtons: string[] = ['Ok']
+
+  constructor(
+    private router: Router
+  ) {
+    const state = this.router.getCurrentNavigation()?.extras.state
+    console.log('state: ' + JSON.stringify(state))
+    if(state){
+      this.nombre = state['nombre']
+    }
+  }
+
+  ngOnInit() {
+  }
+
+  clean(){
+    this.nombre = '';
+    this.apellido = '';
+    this.fechanacimiento = '';
+
+  }
+
+  eventoSeleccion(evento: string){
+    const eventoEncontrado = this.eventos.find(e => e.tipo === evento);
+    if (eventoEncontrado) {
+      this.precio = eventoEncontrado.precio;
+    } else {
+      this.precio = 0;  
+    }
+  }
+
+  getAlertMessage() {
+    return `
+      El valor único es de $${this.precio}
+      El valor total es de $${this.precio}
+
+    `;
+  }
 }
